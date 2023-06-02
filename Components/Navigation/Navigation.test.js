46487a4d0dfd64ae70a547e578ad2c7c979cbc4b
CtzarGM <cesarga4649@google.com>
2023-06-02 15:08:11 +0200

    fix navigation test

--- Components/Navigation/Navigation.test.js
+++ Components/Navigation/Navigation.test.js
@@ -2,7 +2,17 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import Navigation from './Navigation';
 
+jest.mock('next/router', () => ({
+    useRouter() {
+        return {
+            push: jest.fn(),
+            asPath: "/",
+        };
+    },
+}));
+
 describe('Navigation', () => {
+
     it('displays a navigation link labeled "Spotlight"', () => {
         const { getByText } = render(<Navigation />);
         const spotlightLink = getByText('Spotlight');
@@ -15,7 +25,7 @@ describe('Navigation', () => {
         expect(artworksLink).toBeInTheDocument();
     });
 
-    it('displays a navigation link labeled "Artworks"', () => {
+    it('displays a navigation link labeled "Favorites"', () => {
         const { getByText } = render(<Navigation />);
         const favoritesLink = getByText('Favorites');
         expect(favoritesLink).toBeInTheDocument();
